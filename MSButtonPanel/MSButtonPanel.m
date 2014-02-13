86f970b95b0d2264e9c21c51039e38ef3ea90462
Mason Silber <mason@twitter.com>
2014-02-13 15:37:35 -0500

    Added layout subviews and button factory methods

--- MSButtonPanel/MSButtonPanel.m
+++ MSButtonPanel/MSButtonPanel.m
@@ -17,22 +17,57 @@
 
 - (instancetype)initWithButtonTitles:(NSArray *)buttonTitles
 {
-    self = [super initWithFrame:CGRectMake(10, 50, 300, 50)];
+    self = [super initWithFrame:CGRectMake(0, 50, 320, 50)];
     if (self) {
         self.buttons = [NSMutableArray array];
         self.selectedButtonIndex = 0;
         for (NSString *buttonTitle in buttonTitles) {
-            UIButton *newButton = [UIButton buttonWithType:UIButtonTypeCustom];
-            [newButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+            UIButton *newButton = [self _buttonWithTitle:buttonTitle];
             [self.buttons addObject:newButton];
+            [self addSubview:newButton];
         }
     }
     return self;
 }
 
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    CGFloat xOrigin = 10.0f;
+    for (UIButton *button in self.buttons) {
+        if ([self.buttons indexOfObject:button] == self.selectedButtonIndex) {
+            button.frame = CGRectMake(xOrigin, 3.0f, [self _selectedButtonWidth], 44.0f);
+            xOrigin += 10.0f + [self _selectedButtonWidth];
+        } else {
+            button.frame = CGRectMake(xOrigin, 3.0f, [self _normalButtonWidth], 44.0f);
+            xOrigin += 10.0f + [self _normalButtonWidth];
+        }
+    }
+}
+
+- (UIButton *)_buttonWithTitle:(NSString *)title
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    button.backgroundColor = [UIColor blackColor];
+    button.titleLabel.text = title;
+    button.titleLabel.textColor = [UIColor whiteColor];
+    return button;
+}
+
 - (void)buttonPressed:(id)sender
 {
     self.selectedButtonIndex = [self.buttons indexOfObject:sender];
+}
+
+- (CGFloat)_normalButtonWidth
+{
+    return 0.0f;
+}
+
+- (CGFloat)_selectedButtonWidth
+{
+    return 0.0f;
 }
 
 @end
