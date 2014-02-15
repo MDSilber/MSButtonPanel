3b6632e4faf8aadb1a12ecc512437b53ae0ba1ce
Mason Silber <mason@twitter.com>
2014-02-15 16:09:18 -0500

    Set colors of buttons

--- MSButtonPanel-Example/MSButtonPanel-Example/ViewController.m
+++ MSButtonPanel-Example/MSButtonPanel-Example/ViewController.m
@@ -15,8 +15,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     MSButtonPanel *buttonPanel = [[MSButtonPanel alloc] initWithButtonTitles:@[@"Test", @"Test", @"Test", @"Test", @"Test"]];
+    buttonPanel.selectedTextColor = [UIColor whiteColor];
+    buttonPanel.unselectedTextColor = [UIColor lightGrayColor];
+    buttonPanel.selectedBackgroundColor = [UIColor colorWithRed:53.0f/255.0f green:75.0f/255.0f blue:94.0f/255.0f alpha:1.0f];
+    buttonPanel.unselectedBackgroundColor = [UIColor colorWithRed:71.0f/255.0f green:95.0f/255.0f blue:119.0f/255.0f alpha:1.0f];
     [self.view addSubview:buttonPanel];
 }
 
